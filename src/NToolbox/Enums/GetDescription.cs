0be53c726862dd0598b407048db241cdf143cd81
Leandro Morelato <lmorelato@gmail.com>
2019-04-30 10:58:09 -0400

    Enums/GetDescription.cs

---
+++ src/NToolbox/Enums/GetDescription.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace NToolbox.Enums
+{
+    public static partial class EnumExtensions
+    {
+        public static string GetDescription(this Enum valor)
+        {
+            if (valor == null)
+            {
+                throw new ArgumentNullException(nameof(valor));
+            }
+
+            var field = valor.GetType().GetField(valor.ToString());
+            if (field == null)
+            {
+                return valor.ToString();
+            }
+
+            var attributes = (DescriptionAttribute[])field.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes.First().Description : valor.ToString();
+        }
+    }
+}
